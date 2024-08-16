7f85238397a6fdf21faec4b19e625f070b1425e0
miaodongzhi <83210826+miaodongzhi@users.noreply.github.com>
2024-08-16 13:36:30 +1000

    Now it should be fine
    This is for the week 4

--- dmiao-library/src/components/Form.vue
+++ dmiao-library/src/components/Form.vue
@@ -92,31 +92,26 @@
     </div>
   </div>
 
+  <!-- PrimeVue DataTable -->
   <div class="row mt-5" v-if="submittedCards.length">
-    <div class="d-flex flex-wrap justify-content-start">
-      <div
-        v-for="(card, index) in submittedCards"
-        :key="index"
-        class="card m-2"
-        style="width: 18rem"
-      >
-        <div class="card-header">User Information</div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Username: {{ card.username }}</li>
-          <li class="list-group-item">Password: {{ card.password }}</li>
-          <li class="list-group-item">
-            Australian Resident: {{ card.isAustralian === 'yes' ? 'Yes' : 'No' }}
-          </li>
-          <li class="list-group-item">Gender: {{ card.gender }}</li>
-          <li class="list-group-item">Reason: {{ card.reason }}</li>
-        </ul>
-      </div>
-    </div>
+    <DataTable :value="submittedCards" class="p-datatable">
+      <Column field="username" header="Username"></Column>
+      <Column field="password" header="Password"></Column>
+      <Column field="isAustralian" header="Australian Resident">
+        <template #body="slotProps">
+          {{ slotProps.data.isAustralian === 'yes' ? 'Yes' : 'No' }}
+        </template>
+      </Column>
+      <Column field="gender" header="Gender"></Column>
+      <Column field="reason" header="Reason"></Column>
+    </DataTable>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 const formData = ref({
   username: '',
