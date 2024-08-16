f178448d799ebeaa576dcb09f370b98ebda855cd
miaodongzhi <83210826+miaodongzhi@users.noreply.github.com>
2024-08-16 11:18:08 +1000

    now update the week4
    this is week4 activity 1~3

--- dmiao-library/src/components/Form.vue
+++ dmiao-library/src/components/Form.vue
@@ -8,7 +8,15 @@
           <div class="row mb-3">
             <div class="col-md-6">
               <label for="username" class="form-label">Username</label>
-              <input type="text" class="form-control" id="username" v-model="formData.username" />
+              <input
+                type="text"
+                class="form-control"
+                id="username"
+                @blur="() => validateName(true)"
+                @input="() => validateName(false)"
+                v-model="formData.username"
+              />
+              <div v-if="errors.username" class="text-danger">{{ errors.username }}</div>
             </div>
             <div class="col-md-6">
               <label for="password" class="form-label">Password</label>
@@ -16,29 +24,52 @@
                 type="password"
                 class="form-control"
                 id="password"
+                @blur="() => validatePassword(true)"
+                @input="() => validatePassword(false)"
                 v-model="formData.password"
               />
+              <div v-if="errors.password" class="text-danger">{{ errors.password }}</div>
             </div>
           </div>
           <div class="row mb-3">
             <div class="col-md-6">
+              <label class="form-label">Australian Resident?</label>
               <div class="form-check">
                 <input
-                  type="checkbox"
+                  type="radio"
                   class="form-check-input"
-                  id="isAustralian"
+                  id="isAustralianYes"
+                  value="yes"
                   v-model="formData.isAustralian"
                 />
-                <label class="form-check-label" for="isAustralian">Australian Resident?</label>
+                <label class="form-check-label" for="isAustralianYes">Yes</label>
               </div>
+              <div class="form-check">
+                <input
+                  type="radio"
+                  class="form-check-input"
+                  id="isAustralianNo"
+                  value="no"
+                  v-model="formData.isAustralian"
+                />
+                <label class="form-check-label" for="isAustralianNo">No</label>
+              </div>
+              <div v-if="errors.resident" class="text-danger">{{ errors.resident }}</div>
             </div>
             <div class="col-md-6">
               <label for="gender" class="form-label">Gender</label>
-              <select class="form-select" id="gender" v-model="formData.gender">
+              <select
+                class="form-select"
+                id="gender"
+                v-model="formData.gender"
+                @blur="validateGender"
+              >
+                <option value="" disabled>Select Gender</option>
                 <option value="male">Male</option>
                 <option value="female">Female</option>
                 <option value="other">Other</option>
               </select>
+              <div v-if="errors.gender" class="text-danger">{{ errors.gender }}</div>
             </div>
           </div>
           <div class="mb-3">
@@ -48,7 +79,9 @@
               id="reason"
               rows="3"
               v-model="formData.reason"
+              @blur="validateReason"
             ></textarea>
+            <div v-if="errors.reason" class="text-danger">{{ errors.reason }}</div>
           </div>
           <div class="mb-3">
             <button type="submit" class="btn btn-primary me-2">Submit</button>
@@ -72,7 +105,7 @@
           <li class="list-group-item">Username: {{ card.username }}</li>
           <li class="list-group-item">Password: {{ card.password }}</li>
           <li class="list-group-item">
-            Australian Resident: {{ card.isAustralian ? 'Yes' : 'No' }}
+            Australian Resident: {{ card.isAustralian === 'yes' ? 'Yes' : 'No' }}
           </li>
           <li class="list-group-item">Gender: {{ card.gender }}</li>
           <li class="list-group-item">Reason: {{ card.reason }}</li>
@@ -83,23 +116,115 @@
 </template>
 
 <script setup>
-// Our logic will go here
 import { ref } from 'vue'
 
 const formData = ref({
   username: '',
   password: '',
-  isAustralian: false,
+  isAustralian: '',
   reason: '',
   gender: ''
 })
 
 const submittedCards = ref([])
 
+const errors = ref({
+  username: null,
+  password: null,
+  resident: null,
+  gender: null,
+  reason: null
+})
+
 const submitForm = () => {
-  submittedCards.value.push({
-    ...formData.value
-  })
+  validateName(true)
+  validatePassword(true)
+  validateResident(true)
+  validateGender(true)
+  validateReason(true)
+
+  if (
+    !errors.value.username &&
+    !errors.value.password &&
+    !errors.value.resident &&
+    !errors.value.gender &&
+    !errors.value.reason
+  ) {
+    submittedCards.value.push({ ...formData.value })
+    clearForm()
+  }
+}
+
+const clearForm = () => {
+  formData.value = {
+    username: '',
+    password: '',
+    isAustralian: '',
+    reason: '',
+    gender: ''
+  }
+  errors.value = {
+    username: null,
+    password: null,
+    resident: null,
+    gender: null,
+    reason: null
+  }
+}
+
+const validateName = (blur) => {
+  if (formData.value.username.length < 3) {
+    if (blur) errors.value.username = 'Name must be at least 3 characters'
+  } else {
+    errors.value.username = null
+  }
+}
+
+const validatePassword = (blur) => {
+  const password = formData.value.password
+  const minLength = 8
+  const hasUppercase = /[A-Z]/.test(password)
+  const hasLowercase = /[a-z]/.test(password)
+  const hasNumber = /\d/.test(password)
+  const hasSpecialChar = /[!@#$%^&*(),.?":{}|<>]/.test(password)
+
+  if (password.length < minLength) {
+    if (blur) errors.value.password = 'Password must be at least 8 characters long.'
+  } else if (!hasUppercase) {
+    if (blur) errors.value.password = 'Password must contain at least one uppercase letter.'
+  } else if (!hasLowercase) {
+    if (blur) errors.value.password = 'Password must contain at least one lowercase letter.'
+  } else if (!hasNumber) {
+    if (blur) errors.value.password = 'Password must contain at least one number.'
+  } else if (!hasSpecialChar) {
+    if (blur) errors.value.password = 'Password must contain at least one special character.'
+  } else {
+    errors.value.password = null
+  }
+}
+
+const validateResident = (blur) => {
+  if (!formData.value.isAustralian) {
+    if (blur) errors.value.resident = 'Please select if you are an Australian Resident.'
+  } else {
+    errors.value.resident = null
+  }
+}
+
+const validateGender = () => {
+  if (!formData.value.gender) {
+    errors.value.gender = 'Please select a gender.'
+  } else {
+    errors.value.gender = null
+  }
+}
+
+const validateReason = () => {
+  if (!formData.value.reason) {
+    errors.value.reason = 'Please provide a reason for joining.'
+  } else {
+    errors.value.reason = null
+  }
 }
 </script>
 
@@ -119,7 +244,6 @@ const submitForm = () => {
   padding: 10px;
 }
 
-/*new breakpoint between 576 and 768*/
 @media (min-width: 576px) and (max-width: 768px) {
   .container.mt-5 .row {
     display: grid;
